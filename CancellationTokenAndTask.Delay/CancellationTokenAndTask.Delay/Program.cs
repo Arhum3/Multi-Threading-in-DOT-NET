c0093971e34b6aaca24e9bf046f4e988e243a83b
Arhum Imran <81429722+Arhum3@users.noreply.github.com>
2023-08-05 04:08:26 +0500

    Cancellation Token and Task.Delay
    added new implementation of cancellation token and Task Delay.

---
+++ CancellationTokenAndTask.Delay/CancellationTokenAndTask.Delay/Program.cs
@@ -0,0 +1,32 @@
+﻿using CancellationTokenAndTaskDelay;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter the interval(in seconds): ");
+        int invl = int.Parse(Console.ReadLine());
+        //converting the interval to milliseconds
+        invl = invl * 1000;
+
+        string s = "";
+
+        Console.WriteLine("Press c to cancel the task\n");
+
+        CancellationTokenSource source = new CancellationTokenSource();
+        CancellationToken token = source.Token;
+
+        var taskRunner = new RepeatingTaskRunner();
+
+        var repeatingTask = taskRunner.RunningTaskRepeatedly(invl, token, 0);
+
+        s = Console.ReadLine();
+
+        if (s == "c")
+            source.Cancel();
+
+        await repeatingTask;
+
+        Console.ReadLine();
+    }
+}
---
+++ CancellationTokenAndTask.Delay/CancellationTokenAndTask.Delay/RepeatingTaskRunner.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CancellationTokenAndTaskDelay
+{
+    public class RepeatingTaskRunner
+    {
+        public RepeatingTaskRunner() { }
+        public async Task RunningTaskRepeatedly(int invl, CancellationToken token, int count)
+        {
+            Console.WriteLine("Task Running...");
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Same Task Running for {0}th time", count++);
+                    await Task.Delay(invl, token);
+                }
+            }
+
+            catch (TaskCanceledException tce)
+            {
+                Console.WriteLine("Unable to proceed further because " + tce.Message);
+            }
+        }
+    }
+}
