874b4367d6e40805fbc47593f6134c942506af7b
DESKTOP-1BOPGNA\Arhum <arhumimran6@gmail.com>
2023-08-11 22:45:21 +0500

    moved all local variables to RepeatingTaskRunner Class and exposed proper start and stop events.

--- CancellationTokenAndTask.Delay/CancellationTokenAndTask.Delay/Program.cs
+++ CancellationTokenAndTask.Delay/CancellationTokenAndTask.Delay/Program.cs
@@ -1,29 +1,43 @@
 ﻿using CancellationTokenAndTaskDelay;
+//changes: move cancellation token in RepeatingTaskrunner class, thus implementing Single Responsibility Principle
+//Add an action method that would simulate some actual work with delay
+//moving all local variables to RepeatingTaskRunner Class
+//also exposing proper start and stop events
 
+//TODO: Implement Logging
 internal class Program
 {
     private static async Task Main(string[] args)
     {
-        Console.WriteLine("Enter the interval(in seconds): ");
-        int invl = int.Parse(Console.ReadLine());
-        //converting the interval to milliseconds
-        invl = invl * 1000;
-
         string s = "";
-
-        Console.WriteLine("Press c to cancel the task\n");
-
-        CancellationTokenSource source = new CancellationTokenSource();
-        CancellationToken token = source.Token;
-
         var taskRunner = new RepeatingTaskRunner();
 
-        var repeatingTask = taskRunner.RunningTaskRepeatedly(invl, token, 0);
+        Console.WriteLine("Press c to cancel the task\n");
+        
+        taskRunner.Start();
+
+        Action<CancellationToken> WorkAction = (CancellationToken) => {
+            
+            Console.WriteLine("Long Work Simulation Started...");
+
+            try
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Long Work Simulation Ended...");
+            }
+            catch (TaskCanceledException tce)
+            {
+                Console.Out.WriteLineAsync(tce.Message + " This Exception occured in long running work simulation, and it is thrown in action method");
+            }
+            
+        };
+
+        var repeatingTask = taskRunner.RunningTaskRepeatedly(WorkAction);
 
         s = Console.ReadLine();
 
         if (s == "c")
-            source.Cancel();
+            taskRunner.Stop();
 
         await repeatingTask;
 
--- CancellationTokenAndTask.Delay/CancellationTokenAndTask.Delay/RepeatingTaskRunner.cs
+++ CancellationTokenAndTask.Delay/CancellationTokenAndTask.Delay/RepeatingTaskRunner.cs
@@ -3,23 +3,56 @@ namespace CancellationTokenAndTaskDelay
 {
     public class RepeatingTaskRunner
     {
-        public RepeatingTaskRunner() { }
-        public async Task RunningTaskRepeatedly(int invl, CancellationToken token, int count)
+        private CancellationTokenSource _cancellationTokenSource;
+        private int invl;
+        private string TaskName;
+        private int instanceCount;
+        public RepeatingTaskRunner() {
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            TaskName = "";
+            instanceCount = 0;
+            invl = 0;
+        }
+        private void onStart() {
+
+            Console.WriteLine("Enter the interval(in seconds): ");
+            invl = int.Parse(Console.ReadLine());
+            //converting the interval to milliseconds
+            invl = invl * 1000;
+
+            Console.WriteLine("Enter the name of the task: ");
+            TaskName = Console.ReadLine();
+        }
+        public void Start() {
+            onStart();
+        }
+        public async Task RunningTaskRepeatedly(Action<CancellationToken>workAction)
         {
-            Console.WriteLine("Task Running...");
+            Console.WriteLine("Repeating Action Running...");
             try
             {
-                while (!token.IsCancellationRequested)
+                while (!_cancellationTokenSource.Token.IsCancellationRequested)
                 {
-                    Console.WriteLine("Same Task Running for {0}th time", count++);
-                    await Task.Delay(invl, token);
+                    Console.WriteLine("\nTask {1} Running for {0}th time", instanceCount++, TaskName);
+                    workAction(_cancellationTokenSource.Token);
+                    await Task.Delay(invl, _cancellationTokenSource.Token);
                 }
             }
 
             catch (TaskCanceledException tce)
             {
-                Console.WriteLine("Unable to proceed further because " + tce.Message);
+                Console.WriteLine("Task {0} Unable to proceed further because " + tce.Message + " This Exception was thrown at RepeatingTaskRunner Class", TaskName);
             }
+        }
+
+        public void Stop()
+        {
+            CancelTask();
+        }
+        private void CancelTask()
+        {
+           _cancellationTokenSource?.Cancel(); 
         }
     }
 }
